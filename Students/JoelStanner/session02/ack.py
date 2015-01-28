8c914c912a90e4456e6d71c4738b9c844f29fef4
poolbath1 <eyftickid@poolbath1.33mail.com>
2015-01-27 18:26:16 -0800

    Added assertion tests

--- Students/JoelStanner/session02/ack.py
+++ Students/JoelStanner/session02/ack.py
@@ -11,7 +11,25 @@ def ack(m, n):
 
 
 if __name__ == '__main__':
-    for m in range(0, 4):
-        for n in range(0, 5):
-            print("m is " + str(m) + " n is " + str(n) +
-                  " = " + str(ack(m, n)))
+    assert ack(0,0) == 1
+    assert ack(0,1) == 2
+    assert ack(0,2) == 3
+    assert ack(0,3) == 4
+    assert ack(0,4) == 5
+    assert ack(1,0) == 2
+    assert ack(1,1) == 3
+    assert ack(1,2) == 4
+    assert ack(1,3) == 5
+    assert ack(1,4) == 6
+    assert ack(2,0) == 3
+    assert ack(2,1) == 5
+    assert ack(2,2) == 7
+    assert ack(2,3) == 9
+    assert ack(2,4) == 11
+    assert ack(3,0) == 5
+    assert ack(3,1) == 13
+    assert ack(3,2) == 29
+    assert ack(3,3) == 61
+    assert ack(3,4) == 125
+    print("All Tests Pass")
+
