e17d1f70e5b250006c4aa16d7e0038eb4a6fcda6
poolbath1 <eyftickid@poolbath1.33mail.com>
2015-01-28 00:10:31 -0800

    rot13 function added, tests are testing

--- Students/JoelStanner/session03/rot13.py
+++ Students/JoelStanner/session03/rot13.py
@@ -1,6 +1,19 @@
+"""session03 task 2"""
 from string import maketrans
 
-rot13 = maketrans(
+trans_table = maketrans(
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
     "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
 )
+
+
+def rot13(string):
+    """Return a string encrypted with the ROT13 cypher."""
+    return string.translate(trans_table)
+
+if __name__ == '__main__':
+    test_string = "Hello. I am a nifty test string"
+
+    assert rot13(test_string) == "Uryyb. V nz n avsgl grfg fgevat"
+    assert rot13("Uryyb. V nz n avsgl grfg fgevat") == test_string
+    print("All tests pass.")
