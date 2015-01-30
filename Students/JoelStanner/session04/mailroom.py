a6829b27220b3e012851321f3198095a651fc9fa
poolbath1 <eyftickid@poolbath1.33mail.com>
2015-01-29 17:27:41 -0800

    moving on in the interest of time. returning later if time permits.

--- Students/JoelStanner/session04/mailroom.py
+++ Students/JoelStanner/session04/mailroom.py
@@ -1,7 +1,61 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+"""
+Mailroom is an app to send out thank you letters and print out reports of 
+donors.
 
+At this point I am goint to put mailroom on the back burner and finish it if I 
+am able to later. 
 """
-Create a dictionary containing “name”, “city”, and “cake” for “Chris” 
-from “Seattle” who likes “Chocolate”.
-"""
+
+import safe_input
+
+while user_input != "Quit"
+
+    def get_user_input(prompt, validator=None):
+        """request input from the user with `prompt` and return the result.
+
+        optionally, validate the input with a function `validator` which must
+        take one argument, the input from the user and must return the input if
+        valid, and None if not valid
+        """
+        reply = None
+        while reply is None:
+            reply = ask_for_input(prompt)
+            if there_is_a_validator:
+                validate_the_reply
+        return reply
+
+
+    def thank_report_quit():
+        """Prompt the user for an action.
+
+        Valid choices are:
+        Thank you note
+        donor report
+        quit
+        """
+        user_input = get_user_input("Thank, Report, or Quit?", 
+                                    validator=valid_input)
+        if user_input == "Thank":
+            get_user_input("Enter a full name or 'list' for a list of donors",
+                            validator=valid_input)
+            if reply == "list":
+                print_list_of_donors()
+                thank_report_quit()
+            elif reply == name_not_in_list:
+                add_name_to_data_structure
+                donation_amount()
+
+        donation_amount()
+
+        elif user_input == "Report":
+        print(donor_name, total_donated, number_of_donations, avg_donation_amt)
+        return to prompt
+
+def donation_amount():
+    donation = get_user_input("how much is the donation?", validator=validator)
+    add_donation_to_donation_history
+    print(thank_you_email)
+    return to prompt
+
