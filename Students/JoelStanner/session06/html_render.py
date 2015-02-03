d0f6c3f68f260715fe49cea98fd6d34655164465
poolbath1 <eyftickid@poolbath1.33mail.com>
2015-02-02 21:37:47 -0800

    rendering working now.

--- Students/JoelStanner/session06/html_render.py
+++ Students/JoelStanner/session06/html_render.py
@@ -5,22 +5,19 @@
 class Element(object):
     """Base element of HTML"""
 
-    indent = 0
-
-    def __init__(self, tag_name="", ind=0, content=""):
-
-        self.tag_name = tag_name
-        self.indent = ind
-        self.content = content
+    tag_name = 'html'
+    indent_lvl = 1
+    content = []
 
     def append(self, content):
         """Append content to the element"""
-        self.content += content
+        self.content.append(content)
         return self.content
 
-    def render(self, file_out, ind=""):
-        """Render tag and string in the content"""
-        print("<", self.tag_name, ">")
-        self.file_out = file_out
-        self.ind = ind
+    def render(self, file_out, ind="  "):
+        """Render tag and string in the content. file_out is a file object"""
+        file_out.write('<' + self.tag_name + '>\n')
+        for line in self.content:
+            file_out.write(ind * self.indent_lvl + line + "\n")
+        file_out.write('<' + self.tag_name + '/>\n')
         return
